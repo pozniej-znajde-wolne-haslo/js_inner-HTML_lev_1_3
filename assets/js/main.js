3acea4b2b947b55be29adc1ca4479c056334b998
Ania <Ania@max.local>
2023-01-04 16:45:03 +0100

    single inverted commas

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,19 @@
 
 let gallery = document.getElementById("gallery");
 
-gallery.innerHTML += `<figure><img src="https://picsum.photos/300/300" alt="picture"><figcaption>Fig.1</figcaption></figure>` 
-+ `<figure><img src="https://picsum.photos/300/300" alt="picture"><figcaption>Fig.2</figcaption></figure>` 
-+ `<figure><img src="https://picsum.photos/300/300" alt="picture"><figcaption>Fig.3</figcaption></figure>"`;
+/* gallery.innerHTML += `<figure><img src="https://picsum.photos/300/300" alt="pic"><figcaption>Fig.1</figcaption></figure>` 
++ `<figure><img src="https://picsum.photos/300/300" alt="pic"><figcaption>Fig.2</figcaption></figure>` 
++ `<figure><img src="https://picsum.photos/300/300" alt="pic"><figcaption>Fig.3</figcaption></figure>`;
 
-
+ */
 console.log(gallery);
 
 gallery.style.display = "flex";
 gallery.style.justifyContent = "space-evenly";
+
+gallery.innerHTML += '<figure><img src="https://picsum.photos/300/300" alt="pic"><figcaption>Fig.1</figcaption></figure>' 
++ '<figure><img src="https://picsum.photos/300/300" alt="pic"><figcaption>Fig.2</figcaption></figure>' 
++ '<figure><img src="https://picsum.photos/300/300" alt="pic"><figcaption>Fig.3</figcaption></figure>';
+
+
+// ==== if inverted commas twice - dann einmal '' und einmal "" ====
