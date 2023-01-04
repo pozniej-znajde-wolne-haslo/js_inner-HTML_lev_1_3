041baced53fa165bf3d82379e6cf976c25d98694
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-04 14:01:47 +0100

    js_inner html_lev_1_3

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// ===== SEITEN zum Thema ===
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+// https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+
+
+
+let gallery = document.getElementById("gallery");
+
+gallery.innerHTML += `<figure><img src="https://picsum.photos/300/300" alt="picture"><figcaption>Fig.1</figcaption></figure>` 
++ `<figure><img src="https://picsum.photos/300/300" alt="picture"><figcaption>Fig.2</figcaption></figure>` 
++ `<figure><img src="https://picsum.photos/300/300" alt="picture"><figcaption>Fig.3</figcaption></figure>"`;
+
+
+console.log(gallery);
+
+gallery.style.display = "flex";
+gallery.style.justifyContent = "space-evenly";
